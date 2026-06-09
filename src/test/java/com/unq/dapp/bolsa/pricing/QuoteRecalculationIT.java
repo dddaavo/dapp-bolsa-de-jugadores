f56ccea5187b19d71b9c8710350f3cff533898d2
Anto Pezzola <antonella.pezzola@kitbash3d.com>
2026-06-08 23:12:48 -0300

    Revert "fix: agrega Content-Type JSON en QuoteRecalculationIT para POST sin body"
    This reverts commit cf9e96869a49f9d0a65ec9e3fbf38196e9fb4651.

--- src/test/java/com/unq/dapp/bolsa/pricing/QuoteRecalculationIT.java
+++ src/test/java/com/unq/dapp/bolsa/pricing/QuoteRecalculationIT.java
@@ -116,7 +116,6 @@ class QuoteRecalculationIT {
     private HttpEntity<Void> requestConToken(String token) {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
-        headers.setContentType(MediaType.APPLICATION_JSON);
         return new HttpEntity<>(headers);
     }
 }
